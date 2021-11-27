52164f9209a88cae1b866f61cc9a01c6fee9940f
Seimen <code@seimenadventure.de>
2021-11-27 10:34:00 +0100

    update of setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+import setuptools
 from setuptools import setup
 
 setup(
     name='pyOnvista',
-    version='0.4a',
-    packages=['pyOnvista'],
-    url='',
-    license_files=('LICENSE',),
+    version='0.46a',
+    url="https://github.com/cloasdata/pyOnvista",
+    license='MIT',
+    license_files = ('LICENSE',),
     author='Simon Bauer',
     author_email='code@seimenadventure.de',
-    description='A tiny API for onvista.de financial website.'
+    description='A tiny API for onvista.de financial website.',
+    packages=['pyOnvista'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires = [
+            "requests~=2.26.0",
+            'lxml~=4.6.4',
+    ],
+    package_data={'pyOnvista': ['markets.json']}
 )
